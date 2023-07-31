4ddbc63eadce0ad018c10502d5b83768f8b0977c
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-07-31 03:04:47 +0200

    Aply logout functionalities

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,14 +1,30 @@
 import React from "react";
+import axios from "axios";
+import { useNavigate, useOutletContext } from "react-router-dom";
+
 
 const Dashboard = () => {
- 
+  const outlet = useOutletContext()
+  const navigate = useNavigate()
+ const handleLogout = async() => {
+  try {
+    const response = await axios.post('http://localhost:5000/api/user/logout', {withCredentials: true})
+    let success = response.data.success
+      if(success) {
+        navigate('/login')
+      }
+  }
+  catch(err) {
+    console.log(err)
+  }
+ }
   return (
     <div className="dashboard">
-      <h1>Welcome to My Dashboard</h1>
+      <h1>Welcome back {outlet.userName}</h1>
       <nav>
         <ul className="links">
           <li>
-            <button>Logout</button>
+            <button onClick={handleLogout}>Logout</button>
           </li>
         </ul>
       </nav>
